334d53f06381656ef6577f5a08e4c2ce2f986e90
LisbethRmrz <lisbethrmrz98@gmail.com>
2022-04-04 18:05:12 -0600

    Estructura basica del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'reto01';
+  title = 'Dia 4 del reto'; //Ejemplos de interpolacion (One way data binding)
+  url = 'https://static01.nyt.com/images/2017/02/15/science/15tb-flowers01/15tb-flowers01-superJumbo.jpg'
+  name = ''; //Ejemplos de interpolacion (Two way data binding)
+  texto = 'Hola'; //Ejemplos de interpolacion (One way data binding)
+
+  getName():void{
+    console.log("Hola Lisbeth");
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from '../material.module';
 
 import { AppComponent } from './app.component';
+import { ButtonComponent } from './button/button.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { ProductsComponent } from './pages/products/products.component';
+import { ProductComponent } from './pages/products/product/product.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ButtonComponent,
+    HeaderComponent,
+    ProductsComponent,
+    ProductComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot([
+      { path: 'productos', component: ProductsComponent },
+      { path: '**', redirectTo: '', pathMatch: 'full'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
