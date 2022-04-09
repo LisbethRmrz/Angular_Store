c6e82e6f0b89d273c46c4d09300c76a829193361
LisbethRmrz <lisbethrmrz98@gmail.com>
2022-04-09 15:26:03 -0600

    Agregando rutas en header con routerLink

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
  
-
+constructor(private router: Router){}
 
   ngOnInit(): void {
   }
 
+  goToCheckout():void{
+    this.router.navigate(['/checkout']); //Agregando ruta
+  }
 }
